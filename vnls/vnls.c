7f9eef54d7860ddd80592b0ab228f0bc00a74bdd
saprophytex <stashevsky.e1509@gmail.com>
2025-12-01 23:15:08 +0300

    string util module

---
+++ vnls/vnls.c
@@ -0,0 +1,612 @@
+
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <stdint.h>
+#define VNLS_NO_GENERICS
+#include "vnls.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define min(a, b) ((a) < (b)) ? (a) : (b)
+#define max(a, b) ((a) > (b)) ? (a) : (b)
+
+
+
+
+// 'Safer' version of standard `malloc`. Panics if `malloc` returned `NULL`.
+// Therefore the result of this function is a valid pointer.
+void *vnls_malloc(size_t size);
+
+// 'Safer' version of standard `realloc`. Panics if `realloc` returned `NULL`.
+// Therefore the result of this function is a valid pointer.
+// On failure, frees `ptr`, and only then panics.
+void *vnls_realloc(void *ptr, size_t new_size);
+
+// 'Safer' version of standard `free`.
+// If `NULL` is passed, does nothing.
+void vnls_free(void *ptr);
+
+
+void vnls_panicf(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    fprintf(stderr, "\033[31m");
+    vfprintf(stderr, fmt, args);
+    fprintf(stderr, "\033[0m");
+    va_end(args);
+    abort();
+}
+
+
+
+size_t vnls_align(size_t x) {
+    const size_t WORD_SIZE = sizeof(size_t);
+    return (x + (WORD_SIZE - 1)) & ~(WORD_SIZE - 1);
+}
+
+
+void *vnls_malloc(size_t size) {
+    void *ptr = malloc(size);
+    if (ptr == NULL) {
+        vnls_panicf("Cannot vnls_malloc!\n");
+    }
+    memset(ptr, 0, size);
+    return ptr;
+}
+
+void *vnls_realloc(void *ptr, size_t new_size) {
+    void *new_ptr = realloc(ptr, new_size);
+    if (new_ptr == NULL) {
+        vnls_free(ptr);
+        vnls_panicf("Cannot vnls_realloc!\n");
+    }
+    return new_ptr;
+}
+
+void vnls_free(void *ptr) {
+    if (ptr != NULL) {
+        free(ptr);
+    }
+}
+
+
+
+
+
+
+
+
+
+VnlS_String __vnls_do_nothing(VnlS_String str) {
+    return str;
+}
+
+
+bool vnls_is_empty(VnlS_String str) {
+    return str.length == 0;
+}
+
+
+bool vnls_eq(VnlS_String stra, VnlS_String strb) {
+    if (stra.length != strb.length) {
+        return false;
+    }
+
+    return memcmp(
+        stra.chars,
+        strb.chars,
+        stra.length
+    ) == 0;
+}
+
+
+VnlS_String vnls_substr(VnlS_String str, size_t start, size_t length) {
+    str = vnls_lshift(str, start);
+    if (str.length > length) {
+        str = vnls_rshift(str, str.length - length);
+    }
+    return str;
+}
+
+
+VnlS_String vnls_lshift(VnlS_String str, size_t shift) {
+    shift = min(str.length, shift);
+    str.length -= shift;
+    str.chars  += shift;
+    return str;
+}
+
+
+VnlS_String vnls_rshift(VnlS_String str, size_t shift) {
+    shift = min(str.length, shift);
+    str.length -= shift;
+    return str;
+}
+
+
+bool vnls_startswith(VnlS_String str, VnlS_String prefix) {
+    if (str.length < prefix.length) {
+        return false;
+    }
+    VnlS_String true_prefix = vnls_substr(str, 0, prefix.length);
+    return vnls_eq(true_prefix, prefix);
+}
+
+
+bool vnls_endswith(VnlS_String str, VnlS_String suffix) {
+    if (str.length < suffix.length) {
+        return false;
+    }
+    VnlS_String true_suffix = vnls_substr(str, str.length - suffix.length, suffix.length);
+    return vnls_eq(true_suffix, suffix);
+}
+
+
+ptrdiff_t vnls_find(VnlS_String haystack, VnlS_String needle) {
+    if (vnls_is_empty(needle)) {
+        return 0;
+    }
+
+    if (haystack.length < needle.length) {
+        return VNLS_NOTFOUND;
+    }
+
+    for (size_t i = 0; i < haystack.length - needle.length; ++i) {
+        if (vnls_startswith(haystack, needle)) {
+            return i;
+        }
+        haystack = vnls_lshift(haystack, 1);
+    }
+
+    return VNLS_NOTFOUND;
+}
+
+
+ptrdiff_t vnls_rfind(VnlS_String haystack, VnlS_String needle) {
+    if (vnls_is_empty(needle)) {
+        return haystack.length;
+    }
+
+    if (haystack.length < needle.length) {
+        return VNLS_NOTFOUND;
+    }
+
+    for (size_t i = 0; i < haystack.length - needle.length; ++i) {
+        if (vnls_endswith(haystack, needle)) {
+            return i;
+        }
+        haystack = vnls_rshift(haystack, 1);
+    }
+
+    return VNLS_NOTFOUND;
+}
+
+
+VnlS_String vnls_ltrim(VnlS_String str) {
+    for (size_t i = 0; i < str.length; ++i) {
+        char fist_char = str.chars[0];
+        if (isspace(fist_char)) {
+            str = vnls_lshift(str, 1);
+        } else {
+            break;
+        }
+    }
+
+    return str;
+}
+
+
+VnlS_String vnls_rtrim(VnlS_String str) {
+    for (size_t i = 0; i < str.length; ++i) {
+        char last_char = str.chars[str.length - 1];
+        if (isspace(last_char)) {
+            str = vnls_rshift(str, 1);
+        } else {
+            break;
+        }
+    }
+
+    return str;
+}
+
+
+VnlS_String vnls_trim(VnlS_String str) {
+    return vnls_ltrim(
+        vnls_rtrim(str)
+    );
+}
+
+
+VnlS_String vnls_ltrim_chars(VnlS_String str, VnlS_String characters) {
+    char characters_cstr[characters.length + 1];
+    strncpy(characters_cstr, characters.chars, characters.length);
+    characters_cstr[characters.length] = '\0';
+
+    for (size_t i = 0; i < str.length; ++i) {
+        char fist_char = str.chars[0];
+        if (strchr(characters_cstr, fist_char) != NULL) {
+            str = vnls_lshift(str, 1);
+        }
+    }
+
+    return str;
+}
+
+
+VnlS_String vnls_rtrim_chars(VnlS_String str, VnlS_String characters) {
+    char characters_cstr[characters.length + 1];
+    strncpy(characters_cstr, characters.chars, characters.length);
+    characters_cstr[characters.length] = '\0';
+
+    for (size_t i = 0; i < str.length; ++i) {
+        char last_char = str.chars[str.length - 1];
+        if (strchr(characters_cstr, last_char) != NULL) {
+            str = vnls_rshift(str, 1);
+        }
+    }
+
+    return str;
+}
+
+
+VnlS_String vnls_trim_chars(VnlS_String str, VnlS_String characters) {
+    return vnls_ltrim_chars(
+        vnls_rtrim_chars(str, characters),
+        characters
+    );
+}
+
+
+VnlS_String vnls_remprefix(VnlS_String str, VnlS_String prefix) {
+    if (vnls_startswith(str, prefix)) {
+        str = vnls_lshift(str, prefix.length);
+    }
+
+    return str;
+}
+
+
+VnlS_String vnls_remsuffix(VnlS_String str, VnlS_String suffix) {
+    if (vnls_endswith(str, suffix)) {
+        str = vnls_rshift(str, suffix.length);
+    }
+
+    return str;
+}
+
+
+
+
+VnlS_StringBuffer VNLS_TEMP_BUF = {0};
+
+
+VnlS_String vnls_escape(VnlS_String str) {
+    vnls_buf_reset(&VNLS_TEMP_BUF);
+    VnlS_StringBuffer *buffer = &VNLS_TEMP_BUF;
+
+    vnls_buf_push(buffer, '"');
+    for (size_t i = 0; i < str.length; ++i) {
+        char c = str.chars[i];
+        switch (c) {
+        case '\'': vnls_buf_appends(buffer, "\\'"); break;
+        case '\"': vnls_buf_appends(buffer, "\\\""); break;
+        // case '\?': vnls_buf_append(buffer, "\\?"); break;
+        case '\\': vnls_buf_appends(buffer, "\\\\"); break;
+        case '\a': vnls_buf_appends(buffer, "\\a"); break;
+        case '\b': vnls_buf_appends(buffer, "\\b"); break;
+        case '\f': vnls_buf_appends(buffer, "\\f"); break;
+        case '\n': vnls_buf_appends(buffer, "\\n"); break;
+        case '\r': vnls_buf_appends(buffer, "\\r"); break;
+        case '\t': vnls_buf_appends(buffer, "\\t"); break;
+        case '\v': vnls_buf_appends(buffer, "\\v"); break;
+        default:   vnls_buf_push(buffer, c); break;
+        }
+    }
+
+    vnls_buf_push(buffer, '"');
+    vnls_buf_push(buffer, '\0');
+
+    return (VnlS_String){
+        VNLS_TEMP_BUF.chars,
+        VNLS_TEMP_BUF.length,
+    };
+}
+
+
+
+const char *vnls_escapes(VnlS_String str) {
+    return vnls_escape(str).chars;
+}
+
+
+const char *vnls_to_cstr(VnlS_String str) {
+    vnls_buf_reset(&VNLS_TEMP_BUF);
+    vnls_buf_append(&VNLS_TEMP_BUF, str);
+    vnls_buf_push(&VNLS_TEMP_BUF, '\0');
+    return VNLS_TEMP_BUF.chars;
+}
+
+
+
+VnlS_String vnls_from_cstr(const char *cstr) {
+    return (VnlS_String){ cstr, strlen(cstr) };
+}
+
+
+
+int getbase(VnlS_String str) {
+    str = vnls_ltrim(str);
+    if (vnls_startswith(str, vnls_from_cstr("+"))
+     || vnls_startswith(str, vnls_from_cstr("-"))) {
+         str = vnls_lshift(str, 1);
+    }
+
+    int base;
+    if (vnls_startswith(str, vnls_from_cstr("0x"))
+     || vnls_startswith(str, vnls_from_cstr("0X"))) {
+        base = 16;
+    } else if (vnls_startswith(str, vnls_from_cstr("0b"))
+            || vnls_startswith(str, vnls_from_cstr("0B"))) {
+        base = 2;
+    } else if (vnls_startswith(str, vnls_from_cstr("0o"))
+            || vnls_startswith(str, vnls_from_cstr("0O"))) {
+        base = 8;
+    } else {
+        base = 10;
+    }
+
+    return base;
+}
+
+uintmax_t parse_uint(VnlS_String *str, uintmax_t maxval, VnlS_Error *err, bool full) {
+    int base = getbase(*str);
+    VnlS_Error _dummy_error;
+    if (err == NULL) {
+        err = &_dummy_error;
+    }
+
+    char *cstr = vnls_malloc(str->length + 1);
+    memcpy(cstr, str->chars, str->length);
+    cstr[str->length] = '\0';
+    char *end;
+    uintmax_t number = strtoumax(cstr, &end, base);
+
+    int _errno = errno;
+    if (_errno == EINVAL) {
+        *err = VNLS_PARSE_NO_NUMBER;
+    }
+    if (_errno == ERANGE || number > maxval) {
+        *err = VNLS_PARSE_OVERFLOW;
+    }
+
+    if (end == cstr) {
+        *err = VNLS_PARSE_NO_NUMBER;
+    }
+
+    *str = vnls_lshift(*str, end - cstr);
+
+    if (full && !vnls_is_empty(*str)) {
+        *err = VNLS_PARSE_NOT_FULL;
+    }
+
+    vnls_free(cstr);
+    return number;
+}
+
+intmax_t parse_int(VnlS_String *str, intmax_t minval, intmax_t maxval, VnlS_Error *err, bool full) {
+    int base = getbase(*str);
+    VnlS_Error _dummy_error;
+    if (err == NULL) {
+        err = &_dummy_error;
+    }
+
+    char *cstr = vnls_malloc(str->length + 1);
+    memcpy(cstr, str->chars, str->length);
+    cstr[str->length] = '\0';
+    char *end;
+    intmax_t number = strtoimax(cstr, &end, base);
+
+    int _errno = errno;
+    if (_errno == EINVAL) {
+        *err = VNLS_PARSE_NO_NUMBER;
+    }
+
+    if (_errno == ERANGE || number > maxval || number < minval) {
+        if (number > maxval) *err = VNLS_PARSE_OVERFLOW;
+        if (number < minval) *err = VNLS_PARSE_UNDERFLOW;
+        *err = VNLS_PARSE_OVERFLOW;
+    }
+
+    *str = vnls_lshift(*str, end - cstr);
+
+    if (full && !vnls_is_empty(*str)) {
+        *err = VNLS_PARSE_NOT_FULL;
+    }
+
+    vnls_free(cstr);
+    return number;
+}
+
+intmax_t parse_float(VnlS_String *str, double minval, double maxval, VnlS_Error *err, bool full) {
+    VnlS_Error _dummy_error;
+    if (err == NULL) {
+        err = &_dummy_error;
+    }
+
+    char *cstr = vnls_malloc(str->length + 1);
+    memcpy(cstr, str->chars, str->length);
+    cstr[str->length] = '\0';
+    char *end;
+    double number = strtod(cstr, &end);
+
+    int _errno = errno;
+    if (_errno == EINVAL) {
+        *err = VNLS_PARSE_NO_NUMBER;
+    }
+
+    if (_errno == ERANGE || number > maxval || number < minval) {
+        if (number > maxval) *err = VNLS_PARSE_OVERFLOW;
+        if (number < minval) *err = VNLS_PARSE_UNDERFLOW;
+        *err = VNLS_PARSE_OVERFLOW;
+    }
+
+    *str = vnls_lshift(*str, end - cstr);
+
+    if (full && !vnls_is_empty(*str)) {
+        *err = VNLS_PARSE_NOT_FULL;
+    }
+
+    vnls_free(cstr);
+    return number;
+}
+
+
+uint8_t vnls_parse_u8(VnlS_String *str, VnlS_Error *err) { return parse_uint(str, UINT8_MAX, err, false); }
+uint16_t    vnls_parse_u16(VnlS_String *str, VnlS_Error *err) { return parse_uint(str, UINT16_MAX, err, false); }
+uint32_t    vnls_parse_u32(VnlS_String *str, VnlS_Error *err) { return parse_uint(str, UINT32_MAX, err, false); }
+uint64_t    vnls_parse_u64(VnlS_String *str, VnlS_Error *err) { return parse_uint(str, UINT64_MAX, err, false); }
+int8_t      vnls_parse_i8(VnlS_String *str, VnlS_Error *err) { return parse_int(str, INT8_MIN, INT8_MAX, err, false); }
+int16_t     vnls_parse_i16(VnlS_String *str, VnlS_Error *err) { return parse_int(str, INT16_MIN, INT16_MAX, err, false); }
+int32_t     vnls_parse_i32(VnlS_String *str, VnlS_Error *err) { return parse_int(str, INT32_MIN, INT32_MAX, err, false); }
+int64_t     vnls_parse_i64(VnlS_String *str, VnlS_Error *err) { return parse_int(str, INT64_MIN, INT32_MAX, err, false); }
+float       vnls_parse_f32(VnlS_String *str, VnlS_Error *err) { return parse_float(str, FLT_MIN, FLT_MAX, err, false); }
+double      vnls_parse_f64(VnlS_String *str, VnlS_Error *err) { return parse_float(str, DBL_MIN, DBL_MAX, err, false); }
+
+
+uint8_t     vnls_to_u8(VnlS_String *str, VnlS_Error *err) { return parse_uint(str, UINT8_MAX, err, true); }
+uint16_t    vnls_to_u16(VnlS_String *str, VnlS_Error *err) { return parse_uint(str, UINT16_MAX, err, true); }
+uint32_t    vnls_to_u32(VnlS_String *str, VnlS_Error *err) { return parse_uint(str, UINT32_MAX, err, true); }
+uint64_t    vnls_to_u64(VnlS_String *str, VnlS_Error *err) { return parse_uint(str, UINT64_MAX, err, true); }
+int8_t      vnls_to_i8(VnlS_String *str, VnlS_Error *err) { return parse_int(str, INT8_MIN, INT8_MAX, err, true); }
+int16_t     vnls_to_i16(VnlS_String *str, VnlS_Error *err) { return parse_int(str, INT16_MIN, INT16_MAX, err, true); }
+int32_t     vnls_to_i32(VnlS_String *str, VnlS_Error *err) { return parse_int(str, INT32_MIN, INT32_MAX, err, true); }
+int64_t     vnls_to_i64(VnlS_String *str, VnlS_Error *err) { return parse_int(str, INT64_MIN, INT32_MAX, err, true); }
+float       vnls_to_f32(VnlS_String *str, VnlS_Error *err) { return parse_float(str, FLT_MIN, FLT_MAX, err, true); }
+double      vnls_to_f64(VnlS_String *str, VnlS_Error *err) { return parse_float(str, DBL_MIN, DBL_MAX, err, true); }
+
+
+int vnls_chop(VnlS_String *str) {
+    if (vnls_is_empty(*str)) {
+        return VNLS_EMPTY;
+    }
+    char c = str->chars[0];
+    *str = vnls_lshift(*str, 1);
+    return c;
+}
+
+
+
+
+void vnls_buf_reset(VnlS_StringBuffer *buf) {
+    buf->length = 0;
+}
+
+
+
+void vnls_buf_clear(VnlS_StringBuffer *buf) {
+    vnls_free(buf->chars);
+    *buf = (VnlS_StringBuffer){0};
+}
+
+
+void vnls_buf_reserve(VnlS_StringBuffer *buf, size_t extra_size) {
+    size_t min_capacity = buf->length + extra_size;
+    if (buf->capacity < min_capacity) {
+        size_t new_capacity = buf->capacity
+                            ? vnls_align(max(buf->capacity * 2, min_capacity))
+                            : vnls_align(min_capacity);
+        buf->chars = vnls_realloc(buf->chars, new_capacity);
+        buf->capacity = new_capacity;
+    }
+}
+
+
+
+void vnls_buf_reserve_exact(VnlS_StringBuffer *buf, size_t size) {
+    if (buf->capacity < size) {
+        buf->chars = vnls_realloc(buf->chars, size);
+        buf->capacity = size;
+    }
+}
+
+void vnls_buf_push(VnlS_StringBuffer *buf, char c) {
+    vnls_buf_reserve(buf, 1);
+    buf->chars[buf->length++] = c;
+}
+
+void vnls_buf_append(VnlS_StringBuffer *buf, VnlS_String str) {
+    vnls_buf_reserve(buf, str.length);
+    memcpy(buf->chars + buf->length, str.chars, str.length);
+    buf->length += str.length;
+}
+
+void vnls_buf_appends(VnlS_StringBuffer *buf, const char *cstr) {
+    size_t len = strlen(cstr);
+    vnls_buf_reserve(buf, len);
+    memcpy(buf->chars + buf->length, cstr, len);
+    buf->length += len;
+}
+void vnls_buf_appendf(VnlS_StringBuffer *buf, const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+
+    va_list args_copy;
+    va_copy(args_copy, args);
+    size_t extra_size = vsnprintf(NULL, 0, fmt, args_copy);
+    va_end(args_copy);
+
+    // +1 for terminating null
+    vnls_buf_reserve(buf, extra_size + 1);
+    vsnprintf(buf->chars + buf->length, extra_size, fmt, args);
+    buf->length += extra_size;
+    va_end(args);
+}
+
+
+
+const char *vnls_buf_cstr(VnlS_StringBuffer *buf) {
+    // empty or last char is not null
+    if (!buf->length || !buf->chars[buf->length - 1]) {
+        vnls_buf_push(buf, '\0');
+    }
+    return buf->chars;
+}
+
+
+VnlS_String vnls_buf_str(const VnlS_StringBuffer *buf) {
+    return (VnlS_String){ buf->chars, buf->length };
+}
+
+
+bool vnls_read_file(VnlS_StringBuffer *buf, VnlS_String filename) {
+
+    FILE *fp = fopen(vnls_to_cstr(filename), "r");
+    if (!fp) return false;
+
+    const size_t CHUNKSIZE = 1024;
+    rewind(fp);
+    while (true) {
+        vnls_buf_reserve(buf, CHUNKSIZE);
+        // printf("buf [%p, %zu / %zu] -> ", (void *)buf->chars,  buf->length, buf->capacity);
+        char f[1024] = {0};
+        size_t chars_read = fread(
+            // buf->chars + buf->length,
+            f,
+            sizeof(char),
+            CHUNKSIZE,
+            fp);
+        // printf("chars_read=%d, %d", (int)chars_read, feof(fp));
+        // printf("%.*s\n", (int)chars_read, f);
+        // printf("%.*s\n", (int)buf->length, buf->chars);
+        vnls_buf_append(buf, (VnlS_String){f, chars_read});
+
+        if (!chars_read) break;
+    }
+
+    return true;
+}
---
+++ vnls/vnls.h
@@ -0,0 +1,238 @@
+/*
+Macros:
+    VNSL_NO_GENERICS
+
+*/
+
+
+#ifndef VNLS_H
+#define VNLS_H
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+
+
+typedef struct VnlS_String {
+    const char *chars;
+    size_t length;
+} VnlS_String;
+
+
+
+typedef struct VnlS_StringBuffer {
+    char *chars;
+    size_t length;
+    size_t capacity;
+} VnlS_StringBuffer;
+
+
+typedef enum VnlS_Error {
+    VNLS_PARSE_UNDERFLOW,
+    VNLS_PARSE_OVERFLOW,
+    VNLS_PARSE_NO_NUMBER,
+    VNLS_PARSE_NOT_FULL,
+} VnlS_Error;
+
+
+#define VNLS_EMPTY (-1)
+#define VNLS_NOTFOUND (-1)
+
+
+
+VnlS_String __vnls_do_nothing(VnlS_String);
+
+// Check is `str` is empty.
+bool vnls_is_empty(VnlS_String str);
+
+// Check if two strings are equal.
+bool vnls_eq(VnlS_String stra, VnlS_String strb);
+
+// Get a substring of `str` at half-open interval `[start, start + length)`.
+// First, lshifts to the `start`. Second, rshifts to strip the length down to `length`.
+// This ensures that in any case resulting substring is 'part' of the input string.
+VnlS_String vnls_substr(VnlS_String str, size_t start, size_t length);
+
+// Left-shift `str` by `min(shift, str.length)` characters.
+// Single shift is done as follows:
+// ```
+//      str.length -= 1;
+//      str.chars  += 1;
+// ```
+// Therefore, it is always safe to work with shifted string as part of the original string.
+VnlS_String vnls_lshift(VnlS_String str, size_t shift);
+
+// Right-shift `str` by `min(shift, str.length)` characters.
+// Single shift is done as follows:
+// ```
+//      str.length -= 1;
+// ```
+// Therefore, it is always safe to work with shifted string as part of the original string.
+VnlS_String vnls_rshift(VnlS_String str, size_t shift);
+
+// Check if `str` starts with `prefix`.
+bool vnls_startswith(VnlS_String str, VnlS_String prefix);
+
+// Check if `str` ends with `suffix`.
+bool vnls_endswith(VnlS_String str, VnlS_String suffix);
+
+// Find the first occurence of `needle` in the `haystack`.
+// Returns index of `needle` or `vnls_NOTFOUND` if not found.
+ptrdiff_t vnls_find(VnlS_String haystack, VnlS_String needle);
+
+// Find the last occurence of `needle` in the `haystack`.
+// Returns index of `needle` or `vnls_NOTFOUND` if not found.
+ptrdiff_t vnls_rfind(VnlS_String haystack, VnlS_String needle);
+
+// Remove leading whitespace characters.
+VnlS_String vnls_ltrim(VnlS_String str);
+
+// Remove trailing whitespace characters.
+VnlS_String vnls_rtrim(VnlS_String str);
+
+// Remove both leading and trailing whitespace characters.
+VnlS_String vnls_trim(VnlS_String str);
+
+// Remove leading characters from specified set.
+VnlS_String vnls_ltrim_chars(VnlS_String str, VnlS_String characters);
+
+// Remove trailing characters from specified set.
+VnlS_String vnls_rtrim_chars(VnlS_String str, VnlS_String characters);
+
+// Remove both leading and trailing characters from specified set.
+VnlS_String vnls_trim_chars(VnlS_String str, VnlS_String characters);
+
+// Remove prefix `prefix` from `str`, if has one.
+VnlS_String vnls_remprefix(VnlS_String str, VnlS_String prefix);
+
+// Remove suffix `prefix` from `str`, if has one.
+VnlS_String vnls_remsuffix(VnlS_String str, VnlS_String suffix);
+
+// Creates escaped version of `str` in temporary string buffer and returns the resulting string.
+// Note that resulting string is not null-terminated.
+VnlS_String vnls_escape(VnlS_String str);
+
+// Creates escaped version of `str` in temporary string buffer,
+// adds terminating null character and returns the resulting string.
+const char *vnls_escapes(VnlS_String str);
+
+// Creates null-terminated version of `str` in temporary string buffer and returns the resulting string.
+const char *vnls_to_cstr(VnlS_String str);
+
+// Create `VnlS_String` from null-terminated string.
+// Null terminator is not encluded in resulting string.
+VnlS_String vnls_from_cstr(const char *);
+
+// Try parse number type from string.
+uint8_t     vnls_parse_u8(VnlS_String *, VnlS_Error *);
+uint16_t    vnls_parse_u16(VnlS_String *, VnlS_Error *);
+uint32_t    vnls_parse_u32(VnlS_String *, VnlS_Error *);
+uint64_t    vnls_parse_u64(VnlS_String *, VnlS_Error *);
+int8_t      vnls_parse_i8(VnlS_String *, VnlS_Error *);
+int16_t     vnls_parse_i16(VnlS_String *, VnlS_Error *);
+int32_t     vnls_parse_i32(VnlS_String *, VnlS_Error *);
+int64_t     vnls_parse_i64(VnlS_String *, VnlS_Error *);
+float       vnls_parse_f32(VnlS_String *, VnlS_Error *);
+double      vnls_parse_f64(VnlS_String *, VnlS_Error *);
+
+// Try parse number type, consuming the whole string.
+uint8_t     vnls_to_u8(VnlS_String *, VnlS_Error *);
+uint16_t    vnls_to_u16(VnlS_String *, VnlS_Error *);
+uint32_t    vnls_to_u32(VnlS_String *, VnlS_Error *);
+uint64_t    vnls_to_u64(VnlS_String *, VnlS_Error *);
+int8_t      vnls_to_i8(VnlS_String *, VnlS_Error *);
+int16_t     vnls_to_i16(VnlS_String *, VnlS_Error *);
+int32_t     vnls_to_i32(VnlS_String *, VnlS_Error *);
+int64_t     vnls_to_i64(VnlS_String *, VnlS_Error *);
+float       vnls_to_f32(VnlS_String *, VnlS_Error *);
+double      vnls_to_f64(VnlS_String *, VnlS_Error *);
+
+
+int vnls_chop(VnlS_String *);
+
+
+
+// Reset buffer
+// Basically sets length to zero.
+void vnls_buf_reset(VnlS_StringBuffer *);
+
+// Clear buffer.
+// Deallocates memory and sets all fields to zeros.
+void vnls_buf_clear(VnlS_StringBuffer *);
+
+// Reserve at least `extra_size` additional elements
+void vnls_buf_reserve(VnlS_StringBuffer *, size_t extra_size);
+
+// Reserve exact `size` elements.
+// If there are more that `size` elements, does nothing.
+void vnls_buf_reserve_exact(VnlS_StringBuffer *, size_t size);
+
+void vnls_buf_push(VnlS_StringBuffer *, char);
+void vnls_buf_append(VnlS_StringBuffer *, VnlS_String);
+void vnls_buf_appends(VnlS_StringBuffer *, const char *);
+void vnls_buf_appendf(VnlS_StringBuffer *, const char *fmt, ...);
+
+// Ensures that contents of the buffer form a null-terminated string
+// by adding null in the end, if needed
+const char *vnls_buf_cstr(VnlS_StringBuffer *);
+
+// Get string slice of all contents of the buffer
+VnlS_String vnls_buf_str(const VnlS_StringBuffer *);
+
+// Read file contents to string buffer
+bool vnls_read_file(VnlS_StringBuffer *, VnlS_String filename);
+
+
+
+
+#ifndef VNLS_NO_GENERICS
+
+#define VnlS_String(anystr)                 \
+    _Generic((anystr),                      \
+        char *: vnls_from_cstr,             \
+        VnlS_String: __vnls_do_nothing,     \
+        VnlS_StringBuffer *: vnls_buf_str   \
+    )(anystr)
+
+#define vnls_is_empty(str)              vnls_is_empty(VnlS_String(str))
+#define vnls_eq(stra, strb)             vnls_eq(VnlS_String(stra), VnlS_String(strb))
+#define vnls_substr(str, start, length) vnls_substr(VnlS_String(str), start, length)
+#define vnls_lshift(str, shift)         vnls_lshift(VnlS_String(str), shift)
+#define vnls_rshift(str, shift)         vnls_rshift(VnlS_String(str), shift)
+#define vnls_startswith(str, prefix)    vnls_startswith(VnlS_String(str), VnlS_String(prefix))
+#define vnls_endswith(str, suffix)      vnls_endswith(VnlS_String(str), VnlS_String(suffix))
+#define vnls_find(haystack, needle)     vnls_find(VnlS_String(haystack), VnlS_String(needle))
+#define vnls_rfind(heystack, needle)    vnls_rfind(VnlS_String(haystack), VnlS_String(needle))
+#define vnls_ltrim(str)                 vnls_ltrim(VnlS_String(str))
+#define vnls_rtrim(str)                 vnls_rtrim(VnlS_String(str))
+#define vnls_trim(str)                  vnls_trim(VnlS_String(str))
+#define vnls_ltrim_chars(str, chars)    vnls_ltrim_chars(VnlS_String(str), chars)
+#define vnls_rtrim_chars(str, chars)    vnls_rtrim_chars(VnlS_String(str), chars)
+#define vnls_trim_chars(str, chars)     vnls_trim_chars(VnlS_String(str), chars)
+#define vnls_remprefix(str, prefix)     vnls_remprefix(VnlS_String(str), VnlS_String(prefix))
+#define vnls_remsuffix(str, suffix)     vnls_remsuffix(VnlS_String(str), VnlS_String(suffix))
+#define vnls_escape(str)                vnls_escape(VnlS_String(str))
+#define vnls_to_u8(str, num)            vnls_to_u8(VnlS_String(str), num)
+#define vnls_to_u16(str, num)           vnls_to_u16(VnlS_String(str), num)
+#define vnls_to_u32(str, num)           vnls_to_u32(VnlS_String(str), num)
+#define vnls_to_u64(str, num)           vnls_to_u64(VnlS_String(str), num)
+#define vnls_to_i8(str, num)            vnls_to_i8(VnlS_String(str), num)
+#define vnls_to_i16(str, num)           vnls_to_i16(VnlS_String(str), num)
+#define vnls_to_i32(str, num)           vnls_to_i32(VnlS_String(str), num)
+#define vnls_to_i64(str, num)           vnls_to_i64(VnlS_String(str), num)
+#define vnls_to_f32(str, num)           vnls_to_f32(VnlS_String(str), num)
+#define vnls_to_f64(str, num)           vnls_to_f64(VnlS_String(str), num)
+
+#define vnls_buf_append(buf, item)                          \
+    _Generic((item),                                        \
+        char: vnls_buf_push(buf, item),                     \
+        default: vnls_buf_append(buf, VnlS_String(item))    \
+    )
+
+#define vnls_read_file(buf, filename) vnls_read_file(buf, VnlS_String(filename))
+
+
+#endif // !VNSL_NO_GENERICS
+
+#endif // VNLSTR_H
