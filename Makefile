bbd32d68b99a8131afa837d20ec3f66651a90d3f
saprophytex <stashevsky.e1509@gmail.com>
2025-12-03 19:40:20 +0300

    Day 3. Naive solution

--- Makefile
+++ Makefile
@@ -1,7 +1,5 @@
-
-
-CC = clang
-CFLAGS = -Wall -Wextra -Wpedantic -std=c23 -O3
+CC		= clang
+CFLAGS	= -Wall -Wextra -Wpedantic -std=c23 -O3
 STRINGS = ../vnls/vnls.c
 
 
@@ -22,4 +20,12 @@ day2-2: day2/part2.c
 	@ cd day2 && $(CC) $(CFLAGS) $(STRINGS) part2.c -o part2
 	@ cd day2 && ./part2
 
-.PHONY: day1-1 day1-2 day2-1 day2-2
+day3-1: day3/part1.c
+	@ cd day3 && $(CC) $(CFLAGS) $(STRINGS) part1.c -o part1
+	@ cd day3 && ./part1
+
+day3-2: day3/part2.c
+	@ cd day3 && $(CC) $(CFLAGS) $(STRINGS) part2.c -o part2
+	@ cd day3 && ./part2
+
+.PHONY: day1-1 day1-2 day2-1 day2-2 day3-1 day3-2
---
+++ day3/part1.c
@@ -0,0 +1,78 @@
+
+#include <stddef.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdint.h>
+#include <limits.h>
+#include "../vnls/vnls.h"
+#include <unistd.h>
+
+
+// Get index of max digit in given range
+size_t max_digit(VnlS_String range) {
+    char max = range.chars[0];
+    size_t idx = 0;
+
+    for (size_t i = 0; i < range.length; ++i) {
+        char curr = range.chars[i];
+        if (curr > max) {
+            max = curr;
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+
+uint64_t solution(VnlS_String input) {
+    const size_t NUMDIGITS = 2;
+
+    uint64_t total_joltage = 0;
+    while (input.length) {
+        VnlS_Partition partition = vnls_partition(input, "\n");
+        VnlS_String bank = partition.before;
+        input = partition.after;
+
+        uint64_t joltage = 0;
+        size_t start = 0;
+        size_t end = bank.length - NUMDIGITS + 1;
+
+        for (size_t i = 0; i < NUMDIGITS; ++i) {
+            size_t digit_idx = start + max_digit(vnls_substr(bank, start, end - start));
+            uint64_t digit = bank.chars[digit_idx] - '0';
+            joltage = joltage * 10 + digit;
+            start = digit_idx + 1;
+            end++;
+        }
+        total_joltage += joltage;
+    }
+
+    return total_joltage;
+}
+
+
+#define BILLION 1'000'000'000.0
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
+
+
+int main() {
+    struct timespec start, end;
+
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    VnlS_StringBuffer buffer = {0};
+    vnls_read_file(&buffer, "input.txt");
+    uint64_t count = solution(VnlS_String(&buffer));
+
+    clock_gettime(CLOCK_REALTIME, &end);
+
+    // time_spent = end - start
+    double time_spent = (end.tv_sec - start.tv_sec) +
+                        (end.tv_nsec - start.tv_nsec) / BILLION;
+
+    printf("count:\t" GREEN "%lld\n" RESET, count);
+    printf("time:\t" GREEN "%f ms\n" RESET, time_spent * 1000);
+    return 0;
+}
